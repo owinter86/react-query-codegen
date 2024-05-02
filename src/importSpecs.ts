705be63ad278a8df908ea2df91c563ddc74be4b7
Oliver Winter <owinter86@gmail.com>
2024-05-02 10:40:14 +0200

    feat: add generation from url

--- src/importSpecs.ts
+++ src/importSpecs.ts
@@ -9,98 +9,149 @@ import { generateImports } from './generateImports.js';
 import { generateSchemas } from './generateSchemas.js';
 
 export function importSpecs({
+  sourceUrls,
   sourceDirectory,
   exportDirectory,
   apiDirectory,
   headerFilters,
 }: {
-  sourceDirectory: string;
+  sourceUrls?: { name: string; url: string }[];
+  sourceDirectory?: string;
   exportDirectory: string;
   apiDirectory: string;
   headerFilters?: string[];
 }) {
-  readdir(sourceDirectory, async (err, filenames) => {
-    if (err) {
-      console.log(err);
-      throw err;
-    }
-    filenames.forEach(async (filename) => {
+  if (sourceUrls) {
+    sourceUrls.forEach(async (d) => {
+      const data = await fetch(d.url).then((res) => res.text());
+      // data response is raw text, check if it is yaml or json
+      let isYaml = true;
       try {
-        const data = readFileSync(join(process.cwd(), sourceDirectory + '/' + filename), 'utf-8');
-        const { ext } = parse(sourceDirectory + '/' + filename);
-        const format = ['.yaml', '.yml'].includes(ext.toLowerCase()) ? 'yaml' : 'json';
-        let spec = await convertSwaggerFile(data, format);
-
-        const formattedFileName = Case.camel(`useQueries, ${filename.split('.')[0]}`);
-        const schemaName = `${formattedFileName}.schema`;
-        const hooksName = `${formattedFileName}`;
-        const name = filename.split('.')[0];
-        mkdirSync(join(process.cwd(), `${exportDirectory}/${name}`), { recursive: true });
-
-        const operationIds: string[] = [];
-
-        let hooks = '';
-        let schemaImportsArray = [] as string[];
-        let collectedQueryImports = [] as ('query' | 'mutation' | 'infiniteQuery')[];
-        Object.entries(spec.paths).forEach(([route, verbs]: [string, PathItemObject]) => {
-          Object.entries(verbs).forEach(([verb, operation]: [string, OperationObject]) => {
-            if (['get', 'post', 'patch', 'put', 'delete'].includes(verb) && !operation.deprecated) {
-              const { implementation, imports, queryImports } = createHook({
-                operation,
-                verb,
-                route: (spec.basePath || '') + route,
-                operationIds,
-                parameters: verbs.parameters,
-                schemasComponents: spec.components,
-                headerFilters,
-              });
-
-              hooks += implementation;
-              imports.forEach((element) => {
-                const formattedImport = element.replace('[]', '');
-
-                if (
-                  !schemaImportsArray.includes(formattedImport) &&
-                  element !== 'void' &&
-                  element !== 'string' &&
-                  !element.includes('{') &&
-                  !element.includes('unknown')
-                ) {
-                  schemaImportsArray.push(formattedImport);
-                }
-              });
-              queryImports.forEach((element) => {
-                if (!schemaImportsArray.includes(element)) {
-                  collectedQueryImports.push(element);
-                }
-              });
-            }
-          });
-        });
-
-        const imports = generateImports({
-          apiDirectory,
-          schemaName,
-          schemaImports: schemaImportsArray,
-          queryImports: collectedQueryImports,
-        });
-
-        writeFileSync(join(process.cwd(), `${exportDirectory}/${name}/${hooksName}.tsx`), imports + hooks);
-
-        writeFileSync(
-          join(process.cwd(), `${exportDirectory}/${name}/${schemaName}.tsx`),
-          generateSchemas({ spec })
-        );
-        console.log(
-          chalk.green(`🎉 [${filename}] Your OpenAPI spec has been converted into react query hooks`)
-        );
+        JSON.parse(data);
+        isYaml = false;
       } catch (error) {
-        if ((error as any).code === 'EISDIR') {
-          console.log(chalk.red('nested folder structure not supported'));
-          return;
+        return false;
+      }
+
+      const format = isYaml ? 'yaml' : 'json';
+
+      make({
+        data,
+        format,
+        filename: d.name,
+        exportDirectory,
+        headerFilters,
+        apiDirectory,
+      });
+    });
+  }
+  if (sourceDirectory) {
+    readdir(sourceDirectory, async (err, filenames) => {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+      filenames.forEach(async (filename) => {
+        try {
+          const data = readFileSync(join(process.cwd(), sourceDirectory + '/' + filename), 'utf-8');
+          const { ext } = parse(sourceDirectory + '/' + filename);
+          const format = ['.yaml', '.yml'].includes(ext.toLowerCase()) ? 'yaml' : 'json';
+          make({
+            data,
+            format,
+            filename,
+            exportDirectory,
+            headerFilters,
+            apiDirectory,
+          });
+        } catch (error) {
+          if ((error as any).code === 'EISDIR') {
+            console.log(chalk.red('nested folder structure not supported'));
+            return;
+          }
+          console.log(chalk.red(`[${filename}]:`), chalk.red(error));
         }
-        console.log(chalk.red(`[${filename}]:`), chalk.red(error));
+      });
+    });
+  }
+}
+
+async function make({
+  data,
+  format,
+  filename,
+  exportDirectory,
+  headerFilters,
+  apiDirectory,
+}: {
+  data: string;
+  format: 'yaml' | 'json';
+  filename: string;
+  exportDirectory: string;
+  headerFilters?: string[];
+  apiDirectory: string;
+}) {
+  let spec = await convertSwaggerFile(data, format);
+
+  const formattedFileName = Case.camel(`useQueries, ${filename.split('.')[0]}`);
+  const schemaName = `${formattedFileName}.schema`;
+  const hooksName = `${formattedFileName}`;
+  const name = filename.split('.')[0];
+  mkdirSync(join(process.cwd(), `${exportDirectory}/${name}`), { recursive: true });
+
+  const operationIds: string[] = [];
+
+  let hooks = '';
+  let schemaImportsArray = [] as string[];
+  let collectedQueryImports = [] as ('query' | 'mutation' | 'infiniteQuery')[];
+  Object.entries(spec.paths).forEach(([route, verbs]: [string, PathItemObject]) => {
+    Object.entries(verbs).forEach(([verb, operation]: [string, OperationObject]) => {
+      if (['get', 'post', 'patch', 'put', 'delete'].includes(verb) && !operation.deprecated) {
+        const { implementation, imports, queryImports } = createHook({
+          operation,
+          verb,
+          route: (spec.basePath || '') + route,
+          operationIds,
+          parameters: verbs.parameters,
+          schemasComponents: spec.components,
+          headerFilters,
+        });
+
+        hooks += implementation;
+        imports.forEach((element) => {
+          const formattedImport = element.replace('[]', '');
+
+          if (
+            !schemaImportsArray.includes(formattedImport) &&
+            element !== 'void' &&
+            element !== 'string' &&
+            !element.includes('{') &&
+            !element.includes('unknown')
+          ) {
+            schemaImportsArray.push(formattedImport);
+          }
+        });
+        queryImports.forEach((element) => {
+          if (!schemaImportsArray.includes(element)) {
+            collectedQueryImports.push(element);
+          }
+        });
       }
     });
   });
+
+  const imports = generateImports({
+    apiDirectory,
+    schemaName,
+    schemaImports: schemaImportsArray,
+    queryImports: collectedQueryImports,
+  });
+
+  writeFileSync(join(process.cwd(), `${exportDirectory}/${name}/${hooksName}.tsx`), imports + hooks);
+
+  writeFileSync(
+    join(process.cwd(), `${exportDirectory}/${name}/${schemaName}.tsx`),
+    generateSchemas({ spec })
+  );
+  console.log(chalk.green(`🎉 [${filename}] Your OpenAPI spec has been converted into react query hooks`));
 }
--- test/script.mjs
+++ test/script.mjs
@@ -1,6 +1,12 @@
 import { importSpecs } from '../dist/importSpecs.js';
 
 importSpecs({
+  sourceUrls: [
+    {
+      name: 'location',
+      url: 'https://developer.yourtravis.com/apis/6551f664dcc3f80001c43645/documentation/raw',
+    },
+  ],
   sourceDirectory: 'test/specs-private',
   exportDirectory: 'test/generated',
   apiDirectory: '../../api',
