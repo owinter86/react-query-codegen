e31577760d8cc9683879a86743eec7a6cfb3aa29
Oliver Winter <owinter86@gmail.com>
2024-06-17 08:07:52 +0200

    fix: fix parsing valid files

--- src/importSpecs.ts
+++ src/importSpecs.ts
@@ -55,7 +55,7 @@ export function importSpecs({
         try {
           const data = readFileSync(join(process.cwd(), sourceDirectory + '/' + filename), 'utf-8');
           const { ext } = parse(sourceDirectory + '/' + filename);
-          if (['.yaml', '.yml', '.json'].includes(ext.toLowerCase())) {
+          if (!['.yaml', '.yml', '.json'].includes(ext.toLowerCase())) {
             console.log(chalk.red(`Skipping ${filename} as it is not a valid openapi/swagger file`));
             return;
           }
